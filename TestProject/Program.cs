73caf2d9abe271456a2267c8bfa24d883f04cf93
Chris Zichko <chriszichkocoding@gmail.com>
2024-01-24 17:16:25 -0500

    First test run with hello world

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!\nSincerely,\nZichKoding");
+
+// To build and run in the terminal, `dotnet build` and `dotnet run`.
