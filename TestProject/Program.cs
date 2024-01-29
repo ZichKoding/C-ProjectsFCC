79187f2b762fc28ac61451f2bb00e12060381360
Chris Zichko <chriszichkocoding@gmail.com>
2024-01-28 19:14:46 -0500

    pushing up what I have before starting the next section of this lesson

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!\nSincerely,\nZichKoding");
+﻿// To build and run in the terminal, `dotnet build` and `dotnet run`.
 
-// To build and run in the terminal, `dotnet build` and `dotnet run`.
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!\nSincerely,\nZichKoding");
