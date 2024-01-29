4dfd3099ad6c42750574a2f9a89362af0afbf85d
Chris Zichko <chriszichkocoding@gmail.com>
2024-01-28 19:17:00 -0500

    pushing up my notes for running .NET in the terminal

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,2 @@
-﻿// To build and run in the terminal, `dotnet build` and `dotnet run`.
-
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!\nSincerely,\nZichKoding");
