8709dbe9eede7e41117655fbaa7a6b27f21d0cc5
Chris Zichko <chriszichkocoding@gmail.com>
2024-02-10 13:18:00 -0500

    finished lesson Call methods from the .NET class library using C#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,6 +2,7 @@
 int secondValue = 600;
 int largerValue;
 
+// Math's method Max() is an overloaded method with 13 options. 
 largerValue = Math.Max(firstValue, secondValue);
 
 Console.WriteLine(largerValue);
